0782b985e48e5255a6fe96e82753aaa946509362
João Vicente <up202208586@up.pt>
2024-04-10 09:19:07 +0100

    Current location

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:study_at/components/bottom_nav_bar.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -33,6 +34,15 @@ class _MapPageState extends State<MapPage> {
               urlTemplate:
                   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', // URL do provedor de tiles
               subdomains: ['a', 'b', 'c'],
+            ),
+            CurrentLocationLayer(
+              alignPositionOnUpdate: AlignOnUpdate.always,
+              style: LocationMarkerStyle(
+                marker: const DefaultLocationMarker(
+                ),
+                markerSize: const Size(20, 20),
+                markerDirection: MarkerDirection.heading,
+              ),
             ),
           ]),
     );
