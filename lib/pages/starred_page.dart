afc83a52154740dc55bb666d279bf583289867b2
Rodrigo de Sousa <rodrigodiasdesousa@gmail.com>
2024-04-25 15:34:58 +0100

    vicente's idea, starred works with user v3

--- lib/pages/starred_page.dart
+++ lib/pages/starred_page.dart
@@ -13,6 +13,39 @@ class StarredPage extends StatefulWidget {
 class _StarredPageState extends State<StarredPage> {
   final currentUser = FirebaseAuth.instance.currentUser!;
   final databaseReference = FirebaseDatabase.instance.ref().child("places");
+  List<String> starredPlacesIds = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Retrieve the list of starred places IDs for the current user
+    retrieveStarredPlacesIds();
+  }
+
+  void retrieveStarredPlacesIds() {
+    DatabaseReference userRef = FirebaseDatabase.instance.ref().child("users/${currentUser.email!
+          .replaceAll('.', '_')
+          .replaceAll('@', '_')
+          .replaceAll('#', '_')}");
+    
+    userRef.onValue.listen((event) {
+      DataSnapshot snapshot = event.snapshot;
+      List<String> ids = [];
+      Map<dynamic, dynamic>? userData = snapshot.value as Map<dynamic, dynamic>?;
+
+      if (userData != null) {
+        List<dynamic> stars = userData['stars'];
+        stars.forEach((starId) {
+          ids.add(starId.toString());
+        });
+      }
+
+      setState(() {
+        starredPlacesIds = ids;
+      });
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -40,96 +73,101 @@ class _StarredPageState extends State<StarredPage> {
                 ),
               ),
               SizedBox(height: 20),
-              Expanded(
-                child: StreamBuilder(
-                  stream: databaseReference.onValue,
-                  builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                    if (snapshot.hasData && snapshot.data != null) {
-                      Map<dynamic, dynamic> data = snapshot.data!.snapshot.value;
-                      List<Widget> items = [];
+              starredPlacesIds.isEmpty
+                  ? Center(
+                      child: Text("No starred places"),
+                    )
+                  : Expanded(
+                      child: StreamBuilder(
+                        stream: databaseReference.onValue,
+                        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                          if (snapshot.hasData && snapshot.data != null) {
+                            Map<dynamic, dynamic> data = snapshot.data!.snapshot.value;
+                            List<Widget> items = [];
 
-                      data.forEach((key, value) {
-                        if (value['imageLink'].toString().contains('')) { // CHANGE THIS FOR THE REAL DB, ONLY SHOWS THE ONES WE WANT
-                          items.add(
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => PlacePage(
-                                      imagelink: value['imageLink'],
-                                      name: value['name'],
-                                      markerTags: value['tags'],
+                            data.forEach((key, value) {
+                              // Check if the place is starred
+                              if (starredPlacesIds.contains(key)) {
+                                items.add(
+                                  GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => PlacePage(
+                                            imagelink: value['imageLink'],
+                                            name: value['name'],
+                                            markerTags: value['tags'],
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    child: Container(
+                                      width: 150,
+                                      height: 150,
+                                      margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                                      decoration: BoxDecoration(
+                                        color: Colors.orangeAccent,
+                                        image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: NetworkImage(
+                                            value['imageLink'],
+                                          ),
+                                        ),
+                                        boxShadow: [
+                                          BoxShadow(
+                                            blurRadius: 4,
+                                            color: Color(0x33000000),
+                                            offset: Offset(0, 2),
+                                          )
+                                        ],
+                                        borderRadius: BorderRadius.circular(25),
+                                        border: Border.all(
+                                          color: Colors.orangeAccent,
+                                          width: 2,
+                                        ),
+                                      ),
+                                      child: Stack(
+                                        children: [
+                                          Align(
+                                            alignment: AlignmentDirectional(0, 0.5),
+                                            child: Text(
+                                              value['name'],
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                letterSpacing: 0,
+                                                fontWeight: FontWeight.w500,
+                                                fontStyle: FontStyle.normal,
+                                              ),
+                                            ),
+                                          ),
+                                          Align(
+                                            alignment: AlignmentDirectional(0, 1),
+                                            child: Icon(
+                                              Icons.stars,
+                                              color: Colors.orangeAccent,
+                                              size: 32,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
                                     ),
                                   ),
                                 );
-                              },
-                              child: Container(
-                                width: 150,
-                                height: 150,
-                                margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-                                decoration: BoxDecoration(
-                                  color: Colors.orangeAccent,
-                                  image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: NetworkImage(
-                                      value['imageLink'],
-                                    ),
-                                  ),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      blurRadius: 4,
-                                      color: Color(0x33000000),
-                                      offset: Offset(0, 2),
-                                    )
-                                  ],
-                                  borderRadius: BorderRadius.circular(25),
-                                  border: Border.all(
-                                    color: Colors.orangeAccent,
-                                    width: 2,
-                                  ),
-                                ),
-                                child: Stack(
-                                  children: [
-                                    Align(
-                                      alignment: AlignmentDirectional(0, 0.5),
-                                      child: Text(
-                                        value['name'],
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          letterSpacing: 0,
-                                          fontWeight: FontWeight.w500,
-                                          fontStyle: FontStyle.normal,
-                                        ),
-                                      ),
-                                    ),
-                                    Align(
-                                      alignment: AlignmentDirectional(0, 1),
-                                      child: Icon(
-                                        Icons.stars,
-                                        color: Colors.orangeAccent,
-                                        size: 32,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          );
-                        }
-                      });
+                              }
+                            });
 
-                      return GridView.count(
-                        crossAxisCount: 3, // Number of items per row
-                        children: items,
-                      );
-                    }
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  },
-                ),
-              ),
+                            return GridView.count(
+                              crossAxisCount: 3, // Number of items per row
+                              children: items,
+                            );
+                          }
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        },
+                      ),
+                    ),
             ],
           ),
         ),
